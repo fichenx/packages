bccdae7035b4f3a5db2abfb27531da3597b69a33
fichenx <fichenx@gmail.com>
2023-06-02 12:33:42 +0800

    备份luci-app-filebrowser

---
+++ clone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#luci-app-bypass
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass
+
+#luci-app-filebrowser
+svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
