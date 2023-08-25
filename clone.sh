3ded5fb14f642fad04579645b9839a6fa4603e7f
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2023-08-25 22:44:32 +0800

    添加luci-app-npc

--- clone.sh
+++ clone.sh
@@ -5,3 +5,6 @@ svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass
 
 #luci-app-filebrowser
 svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
+
+#luci-app-npc
+svn export https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-npc
