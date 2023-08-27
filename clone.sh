d2f91839aef65b4ae9de9f72928b376fa510f599
fichenx <fichenx@gmail.com>
2023-08-27 17:27:23 +0800

    添加 luci-app-serverchan

--- clone.sh
+++ clone.sh
@@ -8,3 +8,6 @@ svn export https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 
 #luci-app-npc
 svn export https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-npc
+
+#luci-app-wechatpush
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush luci-app-serverchan
