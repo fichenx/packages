a52929ef8ced3e4ecf7e096915ee34de0779cf7a
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2024-05-27 22:19:30 +0800

    Create main.sh

---
+++ .github/diy/main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+function git_sparse_clone() {
+  branch="$1" rurl="$2" localdir="$3" && shift 3
+  #git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+  git clone -b $branch --single-branch --no-tags --depth 1 --filter=blob:none --no-checkout $rurl $localdir
+  cd $localdir
+  #git sparse-checkout init --cone
+  #git sparse-checkout set $@
+  git checkout $branch -- $@
+  mv -n $@ ../
+  cd ..
+  rm -rf $localdir
+  }
+  
+#luci-app-bypass
+git_sparse_clone master "https://github.com/kiddin9/openwrt-packages" "kiddin9" luci-app-bypass
+
+#luci-app-filebrowser
+git_sparse_clone main "https://github.com/Lienol/openwrt-package" "Lienol" luci-app-filebrowser
+
+#luci-app-npc
+git_sparse_clone main "https://github.com/Hyy2001X/AutoBuild-Packages" "Hyy2001X" luci-app-npc
+
+#luci-app-wechatpush
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush luci-app-serverchan
+
+#luci-app-vssr
+git clone -b master https://github.com/MilesPoupart/luci-app-vssr luci-app-vssr
